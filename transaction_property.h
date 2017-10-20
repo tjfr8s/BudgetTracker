7e3589c5658e7b7119a3f398ae48fdcd191dc05d
Tyler Freitas <tjfr8s@gmail.com>
2017-10-20 16:33:41 -0700

    modify TransProp's overloaded operator<< to return ostream object containing type and property value.

--- transaction_property.h
+++ transaction_property.h
@@ -6,6 +6,7 @@ instead of have multiple member variables of different types
 #ifndef TRANSACTION_PROPERTY_H
 #define TRANSACTION_PROPERTY_H
 #include <iostream>
+#include <vector>
 
 class TransPropBase
 {
@@ -22,8 +23,12 @@ public:
         LABELS,
         MAX_PROP_TYPE
     };
+
+    std::vector<std::string> propTypeString{"date", "description", "description0",
+    "amount", "trans_type", "category", "account name", "labels", "max prop"};
 };
 
+// inherit TransPropBase publicly for use of enum PropType.
 template<class T>
 class TransProp: public TransPropBase
 {
@@ -32,10 +37,15 @@ private:
     PropType m_type;
 public:
     TransProp(T prop, PropType type);
+
+
     T getProp() {return m_prop;}
+    PropType getType() {return static_cast<PropType>(m_type);}
+    std::string getPropTypeString();
+
     friend std::ostream& operator<<(std::ostream& out, TransProp& prop)
     {
-        out << prop.getProp();
+        out << prop.getPropTypeString() << ": " << prop.getProp();
         return out;
     }
 };
@@ -44,6 +54,12 @@ template<class T>
 TransProp<T>::TransProp(T prop, PropType type): m_prop(prop), m_type(type)
 {
 
+}
+
+template<class T>
+std::string TransProp<T>::getPropTypeString()
+{
+    return propTypeString[m_type];
 }
 
 
